cc104bf104045b1a37a7e54db18338418fe5fd49
JaimeDragons22 <101519803+JaimeDragons22@users.noreply.github.com>
2022-04-25 11:28:54 +0200

    Actualizar main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 namespace SpriteKind {
     export const PlatoGanador = SpriteKind.create()
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.PlatoGanador, function (sprite, otherSprite) {
-    game.splash("Tocame Otra Vez,Vamos Tocame!!!!!!")
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.splash("Lo has encontrado!")
     game.over(true)
 })
 game.splash("Fake News")
 game.splash("Encuentra las noticias reales no falsa!")
+game.setDialogFrame(assets.image`Dialogo`)
 tiles.setCurrentTilemap(tilemap`nivel1`)
 let mySprite = sprites.create(assets.image`Character1`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 scene.cameraFollowSprite(mySprite)
-let mySprite2 = sprites.create(assets.image`Win`, SpriteKind.PlatoGanador)
